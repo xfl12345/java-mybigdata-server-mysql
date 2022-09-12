d7e31d152751cb3c8e48ebfbd679f4741d5b3885
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-12 17:50:30 +0800

    测试字节码技术可行性。

--- src/test/java/cc/xfl12345/mybigdata/server/StudyGenerateCode.java
+++ src/test/java/cc/xfl12345/mybigdata/server/StudyGenerateCode.java
@@ -1,14 +1,23 @@
 package cc.xfl12345.mybigdata.server;
 
+import cc.xfl12345.mybigdata.server.mysql.database.mapper.GlobalDataRecordMapper;
+import cc.xfl12345.mybigdata.server.mysql.database.mapper.base.AppTableCurdMapper;
+import cc.xfl12345.mybigdata.server.mysql.database.mapper.base.impl.AbstractAppTableMapper;
+import cc.xfl12345.mybigdata.server.mysql.database.pojo.GlobalDataRecord;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.NamingStrategy;
+import net.bytebuddy.description.annotation.AnnotationDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.jar.asm.Opcodes;
+
+import java.lang.reflect.Type;
 
 public class StudyGenerateCode {
 
-    public static void main(String[] args) {
-        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+    public static void main(String[] args) throws Exception {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 //        FoxyURLClassLoader urlClassLoader = new FoxyURLClassLoader();
 
         DynamicType.Unloaded<?> dynamicType = new ByteBuddy()
@@ -19,10 +28,44 @@ public class StudyGenerateCode {
             })
             .subclass(Object.class)
             .make();
+        Class<?> genClass = dynamicType.load(classLoader).getLoaded();
+        dynamicType.close();
 
-
+        System.out.println(genClass.getCanonicalName());
 //        new ByteArrayClassLoader(Thread.currentThread().getContextClassLoader(), new HashMap<>())
 
+        StudyGenerateCode studyGenerateCode = new StudyGenerateCode();
+        AbstractAppTableMapper<?> mapper = studyGenerateCode.getMapper(GlobalDataRecord.class, GlobalDataRecordMapper.class);
+        System.out.println(mapper.getClass().getCanonicalName());
+        System.out.println(mapper.getTablePojoType().getCanonicalName());
+    }
+
+    @SuppressWarnings("unchecked")
+    public  <T extends AppTableCurdMapper<?>> AbstractAppTableMapper<?> getMapper(Type pojoType, Class<T> mapperType) throws Exception {
+        // Type pojoType = mapperType.getGenericSuperclass();
+        DynamicType.Unloaded<AbstractAppTableMapper<?>> dynamicType = (DynamicType.Unloaded<AbstractAppTableMapper<?>>) new ByteBuddy()
+            .with(new NamingStrategy.AbstractBase() {
+                protected String name(TypeDescription superClass) {
+                    return GlobalDataRecordMapper.class.getPackageName() + ".impl." + mapperType.getSimpleName() + "DynamicImpl";
+                }
+            })
+            .subclass(TypeDescription.Generic.Builder.parameterizedType(AbstractAppTableMapper.class, pojoType).build())
+            .implement(mapperType)
+            .defineMethod(
+                "getTablePojoType",
+                TypeDescription.Generic.Builder.parameterizedType(Class.class, pojoType).build(),
+                Opcodes.ACC_PUBLIC
+            )
+            .withParameters(new Type[]{})
+            .intercept(FixedValue.value(pojoType))
+            .annotateMethod(AnnotationDescription.Builder.ofType(Override.class).build())
+            .make();
+        Class<AbstractAppTableMapper<T>> mapperClass = (Class<AbstractAppTableMapper<T>>) dynamicType.load(getClass().getClassLoader()).getLoaded();
+        dynamicType.close();
+
+        AbstractAppTableMapper<T> mapper = mapperClass.getDeclaredConstructor().newInstance();
+
+        return mapper;
     }
 
 }
