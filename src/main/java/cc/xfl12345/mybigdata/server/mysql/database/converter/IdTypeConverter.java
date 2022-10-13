d47b392c06df5017cde06d073fce73b61bd538cb
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-13 19:11:06 +0800

    重命名泛型类型名称，从 IdType 改为 ID 。

--- src/main/java/cc/xfl12345/mybigdata/server/mysql/database/converter/IdTypeConverter.java
+++ src/main/java/cc/xfl12345/mybigdata/server/mysql/database/converter/IdTypeConverter.java
@@ -5,22 +5,22 @@ import java.util.function.Consumer;
 /**
  * ID 类型数据转换器。
  * 为了应对日后可能更换 ID 类型的情况，提前做好准备。实现最少能量修改代码。
- * @param <IdType>
+ * @param <ID>
  */
-public abstract class IdTypeConverter<IdType> {
-    protected Class<IdType> idTypeClass;
+public abstract class IdTypeConverter<ID> {
+    protected Class<ID> idTypeClass;
 
-    protected IdType id;
+    protected ID id;
 
-    public IdTypeConverter(Class<IdType> cls) {
+    public IdTypeConverter(Class<ID> cls) {
         idTypeClass = cls;
     }
 
-    public IdType convert(Object id) {
+    public ID convert(Object id) {
         return idTypeClass.cast(id);
     }
 
-    public void injectId2Object(Object id, Consumer<IdType> func) {
+    public void injectId2Object(Object id, Consumer<ID> func) {
         func.accept(idTypeClass.cast(id));
     }
 }
