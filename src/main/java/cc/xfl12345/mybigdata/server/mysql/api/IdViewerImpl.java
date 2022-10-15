27db602c4a77c812bd4c3112282f7ffb6b03844a
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-15 11:24:18 +0800

    优化

--- src/main/java/cc/xfl12345/mybigdata/server/mysql/api/IdViewerImpl.java
+++ src/main/java/cc/xfl12345/mybigdata/server/mysql/api/IdViewerImpl.java
@@ -4,22 +4,20 @@ import cc.xfl12345.mybigdata.server.common.api.IdViewer;
 import cc.xfl12345.mybigdata.server.common.appconst.AppConst;
 import cc.xfl12345.mybigdata.server.common.appconst.AppDataType;
 import cc.xfl12345.mybigdata.server.common.data.requirement.DataRequirementPack;
-import cc.xfl12345.mybigdata.server.common.database.error.TableDataException;
 import cc.xfl12345.mybigdata.server.common.database.mapper.TableBasicMapper;
-import cc.xfl12345.mybigdata.server.mysql.appconst.EnumCoreTable;
 import cc.xfl12345.mybigdata.server.mysql.database.converter.AppIdTypeConverter;
 import cc.xfl12345.mybigdata.server.mysql.database.mapper.base.CoreTableCache;
+import cc.xfl12345.mybigdata.server.mysql.database.mapper.base.TableMapperProperties;
 import cc.xfl12345.mybigdata.server.mysql.database.mapper.impl.DaoPack;
 import cc.xfl12345.mybigdata.server.mysql.database.pojo.GlobalDataRecord;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.InitializingBean;
 import org.teasoft.bee.osql.transaction.Transaction;
 import org.teasoft.honey.osql.core.SessionFactory;
 
-import java.util.HashMap;
+import javax.annotation.PostConstruct;
 
-public class IdViewerImpl implements IdViewer, InitializingBean {
+public class IdViewerImpl implements IdViewer {
     @Getter
     @Setter
     protected String fieldCanNotBeNullMessageTemplate = AppConst.FIELD_CAN_NOT_BE_NULL_MESSAGE_TEMPLATE;
@@ -36,23 +34,17 @@ public class IdViewerImpl implements IdViewer, InitializingBean {
 
     protected AppIdTypeConverter idTypeConverter;
 
-    protected HashMap<String, EnumCoreTable> tableNameMap;
-
-    protected void initTableNameMap() {
-        tableNameMap = new HashMap<>(EnumCoreTable.values().length);
-        for (EnumCoreTable coreTable : EnumCoreTable.values()) {
-            tableNameMap.put(coreTable.getName(), coreTable);
-        }
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() throws Exception {
         if (daoPack == null) {
             throw new IllegalArgumentException(fieldCanNotBeNullMessageTemplate.formatted("daoPack"));
         }
 
-        coreTableCache = daoPack.getTableMapperProperties().getCoreTableCache();
-        idTypeConverter = daoPack.getTableMapperProperties().getIdTypeConverter();
+        TableMapperProperties tableMapperProperties = daoPack.getTableMapperProperties();
+        tableMapperProperties.checkProperties();
+
+        coreTableCache = tableMapperProperties.getCoreTableCache();
+        idTypeConverter = tableMapperProperties.getIdTypeConverter();
         globalDataRecordMapper = daoPack.getMapper(GlobalDataRecord.class);
 
         if (globalDataRecordMapper == null) {
@@ -60,27 +52,19 @@ public class IdViewerImpl implements IdViewer, InitializingBean {
                 mapperNotFoundMessageTemplate.formatted(GlobalDataRecord.class.getCanonicalName())
             );
         }
-
-        initTableNameMap();
     }
 
     @Override
     public AppDataType getDataTypeById(Object id) {
         Object tableNameId;
-        String tableNameUUID;
         Transaction transaction = SessionFactory.getTransaction();
         try {
             transaction.begin();
             GlobalDataRecord globalDataRecord = globalDataRecordMapper.selectById(
                 id,
-                new String[]{
-                    GlobalDataRecord.Fields.id,
-                    GlobalDataRecord.Fields.uuid,
-                    GlobalDataRecord.Fields.tableName
-                }
+                new String[]{GlobalDataRecord.Fields.id, GlobalDataRecord.Fields.tableName}
             );
             tableNameId = globalDataRecord.getTableName();
-            tableNameUUID = globalDataRecord.getUuid();
             transaction.commit();
         } catch (RuntimeException e) {
             transaction.rollback();
@@ -90,22 +74,7 @@ public class IdViewerImpl implements IdViewer, InitializingBean {
         AppDataType appDataType = AppDataType.Null;
         String tableName = coreTableCache.getTableNameCache().getKey(idTypeConverter.convert(tableNameId));
         if (tableName != null) {
-            switch (tableNameMap.get(tableName)) {
-                default -> {
-                    throw new TableDataException(
-                        "全局数据记录表出现了不该出现的引用表",
-                        new Object[]{id},
-                        new String[]{tableNameUUID},
-                        tableName
-                    );
-                }
-                case TABLE_SCHEMA_RECORD -> appDataType = AppDataType.JsonSchema;
-                case STRING_CONTENT -> appDataType = AppDataType.String;
-                case BOOLEAN_CONTENT -> appDataType = AppDataType.Boolean;
-                case NUMBER_CONTENT -> appDataType = AppDataType.Number;
-                case GROUP_RECORD -> appDataType = AppDataType.Array;
-                case OBJECT_RECORD -> appDataType = AppDataType.Object;
-            }
+            appDataType = daoPack.getMapperPackByTableName(tableName).getDataType();
         }
 
         return appDataType;
